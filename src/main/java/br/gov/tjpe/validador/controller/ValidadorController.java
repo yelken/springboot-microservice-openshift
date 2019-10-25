57afa800aa5a3805158ceb1e33f60cd20d63409b
Yelken Gonzales <yelkezin@gmail.com>
2019-10-25 15:13:26 -0300

    Commit inicial

---
+++ src/main/java/br/gov/tjpe/validador/controller/ValidadorController.java
@@ -0,0 +1,17 @@
+package br.gov.tjpe.validador.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class ValidadorController {
+
+    @RequestMapping("/isAjuizarProcesso")
+    public Boolean index() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+}
