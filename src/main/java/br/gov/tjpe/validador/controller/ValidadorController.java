4b8f2cd500cd6521a28906f60f4247eeaf23a190
Yelken Gonzales <yelkezin@gmail.com>
2020-06-29 17:36:48 -0300

    Update ValidadorController.java

--- src/main/java/br/gov/tjpe/validador/controller/ValidadorController.java
+++ src/main/java/br/gov/tjpe/validador/controller/ValidadorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
-public cla ValidadorController {
+public class ValidadorController {
 
     @RequestMapping("/isAjuizarProcesso")
     public Boolean index() {
